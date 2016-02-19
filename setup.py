8f114a96568f9173c2131c449f1150a49c38eb5c
Lars Kellogg-Stedman <lars@redhat.com>
2016-02-19 10:06:20 -0500

    teach setup.py about requirements.txt
    Change-Id: Ie1fe6d85ee1a144fe6f320dd631aff63e7dd4764

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_packages
 from gruf import __version__
 
+with open('requirements.txt') as fd:
+    requires = fd.read().splitlines()
+
 setup(name='gruf',
       author='Lars Kellogg-Stedman',
       author_email='lars@oddbit.com',
       url='https://github.com/larsks/gruf',
       version=__version__,
       packages=find_packages(),
+      install_requires=requires,
       package_data={'gruf': [
           'templates/*.j2',
           'templates/*/*.j2']},
